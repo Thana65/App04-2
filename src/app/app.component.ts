4b63d927204e9f7244e543c47035c8c5e4f9debb
Thanachot Thamma <icenon56@gmail.com>
2024-08-27 13:00:21 +0700

    เพิ่ม ion-tabsและion-menu

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AboutPage } from './about/about.page';
+import { NavController } from '@ionic/angular';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor(private a:NavController) {}
+
+  Aboutpage(){
+this.a.navigateForward('/about')
+  }
+}
